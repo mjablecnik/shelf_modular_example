44211a3ead7d004a27fc0be6d0d9342ad235680c
Martin Jablecnik <martin.jablecnik@email.cz>
2021-11-06 19:23:58 +0100

    Init

---
+++ bin/server.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler);
+
+Response _rootHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response _echoHandler(Request request) {
+  final message = request.params['message'];
+  return Response.ok('$message\n');
+}
+
+void main(List<String> args) async {
+  final ip = InternetAddress.anyIPv4;
+
+  final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+
+  final port = int.parse(Platform.environment['PORT'] ?? '9000');
+  final server = await serve(_handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
