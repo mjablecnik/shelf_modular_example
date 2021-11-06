5c70e95a7721e0403aff41da52d9131ccf53abd2
Martin Jablecnik <martin.jablecnik@email.cz>
2021-11-06 23:27:24 +0100

    Add shelf_hotreload

--- bin/server.dart
+++ bin/server.dart
@@ -1,13 +1,22 @@
-import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
 import 'package:shelf_modular/shelf_modular.dart';
+import 'package:shelf_hotreload/shelf_hotreload.dart';
 
 import 'module.dart';
 
 void main(List<String> arguments) async {
   final s = Stopwatch()..start();
+  withHotreload(() async {
+    final handler = Pipeline()
+        .addMiddleware(logRequests())
+        .addHandler(Modular(module: AppModule()));
 
-  final server = await io.serve(Modular(module: AppModule()), '0.0.0.0', 3000);
-  s.stop();
-  print('Server started in ${s.elapsed.inMilliseconds}ms');
-  print('Address: ${server.address.address}:${server.port}');
+    final server = await serve(handler, '0.0.0.0', 3000);
+    print('Server started in ${s.elapsed.inMilliseconds}ms');
+    print('Address: ${server.address.address}:${server.port}');
+    s.stop();
+
+    return server;
+  });
 }
